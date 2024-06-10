025a28931303829a426dfd117d12a52168cd666c
Qaiylah Conway <cqailah@gmail.com>
2024-06-10 13:38:25 +0200

    Update utils.ts
    Added star emoji . . .

--- utils.ts
+++ utils.ts
@@ -1,14 +1,21 @@
+// Element Selection: Retrieves specific DOM elements to manipulate their content later
+// Imports: Imports enums and interfaces to use in the functions
+
 const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 import { LoyaltyUser, Permissions } from './enums'
 import  Review  from './interfaces'
 
+// Functions:
+
+// showReviewTotal: Updates the total review display with the number of reviews, pluralized correctly, and includes the reviewer’s name and a star icon if applicable
 export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
     const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
     reviewTotalDisplay.innerHTML = value.toString() + ' review' + makeMultiple(value) + ' | last reviewed by ' + reviewer + ' ' + iconDisplay    
 }
 
+// populateUser: Updates the display for returning users and the username
 export function populateUser(isReturning : boolean, userName: string ) {
     if (isReturning){
         returningUserDisplay.innerHTML = 'back'
@@ -16,6 +23,7 @@ export function populateUser(isReturning : boolean, userName: string ) {
     userNameDisplay.innerHTML = userName
 }
 
+// showDetails: Conditionally displays the price per night in a given HTML element
 export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
     if (value) {
         const priceDisplay = document.createElement('div')
@@ -24,12 +32,14 @@ export function showDetails(value: boolean | Permissions, element : HTMLDivEleme
     }
 }
 
+// makeMultiple: Provides correct pluralization for the word "review"
 export function makeMultiple(value: number) : string {
     if (value > 1 || value == 0) {
         return 's'
     } else return ''
 }
 
+// getTopTwoReviews: Sorts an array of reviews by their star rating and returns the top two reviews
 export function getTopTwoReviews(reviews : Review[]) : Review[]  {
  const sortedReviews = reviews.sort((a, b) => b.stars - a.stars)
  return sortedReviews.slice(0,2)
